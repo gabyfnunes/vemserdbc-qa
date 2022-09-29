b0250da0638de3f19f884a5586e118f289587be4
Gabriela de Freitas <gabriela.freitas@dbccompany.com.br>
2022-09-29 10:53:52 -0300

    exercicio versao final

--- Módulo 1/Lista 1/src/Exercicio9.java
+++ Módulo 1/Lista 1/src/Exercicio9.java
@@ -18,15 +18,25 @@ public class Exercicio9 {
         System.out.println("Informe o minuto do final do Jogo: ");
         int minutoFinal = leitorUsuario.nextInt();
         leitorUsuario.nextLine();
+        int  duracaoJogoHoras = 0;
+        int  duracaoJogoMinutos =0;
+
+        if(horaFinal<horaInicio) {
+            duracaoJogoHoras = ((24 - horaInicio) + horaFinal);
+            if(minutoFinal<minutoInicio){
+                duracaoJogoMinutos = ((minutoFinal - minutoInicio) % 60)*(-1);
+            }
+            else{
+                duracaoJogoMinutos = ((minutoFinal - minutoInicio) % 60);
+            }
+        }
+        else{
+            duracaoJogoHoras = ((horaFinal-horaInicio));
+            duracaoJogoMinutos = ((minutoFinal - minutoInicio) % 60);
+        }
 
 
-        horaInicio = (horaInicio*60);
-        horaFinal = (horaFinal*60);
-
-        int duracaoJogoHoras = ((horaFinal-horaInicio)/60);
-        int duracaoJogoMinutos = ((minutoFinal-minutoInicio)%60);
-
-            if(duracaoJogoHoras<=24) {
+        if(duracaoJogoHoras<24) {
                 System.out.println("Duração do jogo (hh:mm): "+duracaoJogoHoras + ":" + duracaoJogoMinutos);
 
             }
