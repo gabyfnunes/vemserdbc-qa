a3e2a999a01f1d91d7faa18a01869fa5662ec42a
Gabriela de Freitas <gabriela.freitas@dbccompany.com.br>
2022-10-06 10:53:35 -0300

    correcao homework conta corrente 1

--- Módulo 1/conta-corrente-homework-1/src/Cliente.java
+++ Módulo 1/conta-corrente-homework-1/src/Cliente.java
@@ -5,31 +5,24 @@ public class Cliente{
     public Endereco enderecos[] = new Endereco[2];
 
 
-    public void imprimirContatos(){
-        int aux = 0;
-        for(int i = 0; i < contatos.length; i++){
-            if (contatos[i] != null){
-                contatos[i].imprimirContato();
-                aux++;
+    public void imprimirContato() {
+        if (contatos != null) {
+            for (int i =0; i < contatos.length; i++){
+                if(contatos[i]!=null){
+                    contatos[i].imprimirContato();
+                }
             }
         }
-        if(aux==0){
-            System.out.println("O CONTATO NÃO PODE SER VAZIO!");
+    }
+    public void imprimirEnderecos() {
+        if (enderecos != null) {
+            for (int i = 0; i <enderecos.length; i++){
+                if (enderecos[i]!=null){
+                    enderecos[i].imprimirEndereco();
+            }
         }
     }
-
-   public void imprimirEndereco() {
-       int aux = 0;
-       for(int i = 0; i < enderecos.length; i++){
-           if (enderecos[i] != null){
-               enderecos[i].imprimirEndereco();
-               aux++;
-           }
-       }
-       if(aux==0){
-           System.out.println("O ENDEREÇO NÃO PODE SER VAZIO!");
-       }
-    }
+ }
 
     public void imprimirCliente(){
         System.out.println("Nome: "+nome+"\nCPF: "+cpf);
