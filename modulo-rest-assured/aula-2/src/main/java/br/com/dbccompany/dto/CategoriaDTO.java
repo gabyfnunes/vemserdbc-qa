ff23a725aac85bf8478678f484b76277f149c6da
Gabriela de Freitas <98540615+gabyfnunes@users.noreply.github.com>
2022-11-03 11:12:49 -0300

    Add files via upload

---
+++ modulo-rest-assured/aula-2/src/main/java/br/com/dbccompany/dto/CategoriaDTO.java
@@ -0,0 +1,13 @@
+package br.com.dbccompany.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties
+public class CategoriaDTO {
+
+    private String id;
+    private String name;
+
+}
